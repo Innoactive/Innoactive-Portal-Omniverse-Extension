23d05458ba170ceee6a0c41ac9ae1cc4a1f3bad2
Daniel Seidl <daniel.seidl@innoactive.de>
2024-02-06 17:27:34 +0100

    first working version

--- exts/de.innoactive/de/innoactive/extension.py
+++ exts/de.innoactive/de/innoactive/extension.py
@@ -1,7 +1,14 @@
 import omni.ext
 import omni.ui as ui
+from omni.ui import color as cl
 import omni.kit
-import pyperclip  # Assume this is available for clipboard operations
+import subprocess
+import os
+import webbrowser
+import threading
+from pxr import Usd
+import omni.usd
+from urllib.parse import quote
 
 LABEL_WIDTH = 120
 FIELD_WIDTH = 230
@@ -10,8 +17,8 @@ HEIGHT = 24
 
 SPACING = 4
 
-MODES = ("browser", "VR")
-MODES_TECHNICAL = ("browser", "standalone")
+MODES = ("browser", "VR", "local")
+MODES_TECHNICAL = ("cloud/browser", "cloud/standalone", "local/windows")
 
 # Functions and vars are available to other extension as usual in python: `example.python_ext.some_public_function(x)`
 def some_public_function(x: int):
@@ -26,11 +33,41 @@ class DeInnoactiveExtension(omni.ext.IExt):
     # ext_id is current extension id. It can be used with extension manager to query additional information, like where
     # this extension is located on filesystem.
 
+    def copy_to_clipboard(self, text):
+        try:
+            if os.name == 'nt':  # For Windows
+                subprocess.run('clip', universal_newlines=True, input=text, shell=True)
+            elif os.name == 'posix':
+                if os.system("which pbcopy") == 0:  # macOS has pbcopy
+                    subprocess.run('pbcopy', universal_newlines=True, input=text)
+                elif os.system("which xclip") == 0 or os.system("which xsel") == 0:  # Linux has xclip or xsel
+                    subprocess.run('xclip -selection clipboard', universal_newlines=True, input=text, shell=True)
+                else:
+                    print("Clipboard utilities pbcopy, xclip, or xsel not found.")
+                    return False
+            else:
+                print("Unsupported OS.")
+                return False
+            print("Text copied successfully.")
+            return True
+        except Exception as e:
+            print(f"Failed to copy text: {e}")
+            return False
+
+    def set_notification(self, value):
+        self._notification_label.text = value
+        def delete_notification():
+            self._notification_label.text = ""
+
+        timer = threading.Timer(5, delete_notification)
+        timer.start()
+        
     def update_sharing_link(self):
-        self._sharing_url_model.as_string = self._base_url_model.as_string + "/apps/" + self._app_id_model.as_string + "/launch/cloud/" + self._mode_str_model.as_string
+        args = quote(self._usd_url_model.as_string)
+        self._sharing_url_model.as_string = self._base_url_model.as_string + "/apps/" + self._app_id_model.as_string + "/launch/" + self._mode_str_model.as_string + "?args=" + args
         self._sharing_url_model_label.text = self._sharing_url_model.as_string
 
-    def on_end_edit_string(self, item_model):
+    def on_value_changed(self, item_model):
         self.update_sharing_link()
         pass
 
@@ -41,78 +78,83 @@ class DeInnoactiveExtension(omni.ext.IExt):
 
         self.update_sharing_link()
 
+    def get_current_usd_file(self):
+        # Implement logic to fetch the currently opened USD file path
+        stage = omni.usd.get_context().get_stage()
+        rootLayer = stage.GetRootLayer()
+        file_path = rootLayer.realPath if rootLayer else ""
+        if file_path.startswith("omniverse://"):
+            #self.set_notification(f"USD file URL: {file_path}")
+            return file_path
+        else:
+            #self.set_notification("The USD file URL does not start with 'omniverse://'.")
+            return ""
+        
+    def copy_url(self):
+        # Copy the generated link to clipboard
+        self.copy_to_clipboard(self._sharing_url_model.as_string)
+        self.set_notification("Link copied to clipboard.")
+
+    def open_url(self):
+        # Open URL in web browser
+        webbrowser.open_new_tab(self._sharing_url_model.as_string)
+        self.set_notification("Sharing URL opened in default webbrowser.")
+
+    def on_shutdown(self):
+        print("[de.innoactive] de innoactive shutdown")
+
     def on_startup(self, ext_id):
         print("Innoactive startup")
 
         self._base_url = "https://portal.innoactive.io"
         self._app_id = 1501  # Default app ID, to be provided
-        self._usd_file_link = self.get_current_usd_file()  # Implement this method
+        self._usd_file = self.get_current_usd_file()  # Implement this method
         self._mode = "browser"  # Default to browser stream
         self._generated_link = ""
 
-        self._window = ui.Window("Innoactive Portal", width=400, height=400)
+        self._window = ui.Window("Innoactive Portal", width=600, height=300)
         with self._window.frame:
-            with ui.VStack():
+            with ui.VStack(spacing=10, height=0):
                 with ui.HStack():
                     ui.Label("Base Url", name="base_url", width=LABEL_WIDTH, height=HEIGHT)
                     self._base_url_model = ui.SimpleStringModel()
                     self._base_url_model.as_string = self._base_url
                     ui.StringField(model=self._base_url_model, height=HEIGHT, word_wrap=True)
-                    self._base_url_model_changed = self._base_url_model.subscribe_value_changed_fn(self.on_end_edit_string)
-                ui.Spacer( height=10)
+                    self._base_url_model_changed = self._base_url_model.subscribe_value_changed_fn(self.on_value_changed)
                 
                 with ui.HStack():
                     ui.Label("App ID", name="app_id", width=LABEL_WIDTH, height=HEIGHT)
                     self._app_id_model = ui.SimpleIntModel()
                     self._app_id_model.as_int = self._app_id
                     ui.IntField(model=self._app_id_model, height=HEIGHT)
-                    self._app_id_model_changed = self._app_id_model.subscribe_value_changed_fn(self.on_end_edit_string)
-                ui.Spacer( height=10)
+                    self._app_id_model_changed = self._app_id_model.subscribe_value_changed_fn(self.on_value_changed)
                 
                 with ui.HStack():
-                    ui.Label("Mode", name="mode", width=LABEL_WIDTH, height=HEIGHT)
+                    ui.Label("Streaming Mode", name="mode", width=LABEL_WIDTH, height=HEIGHT)
                     self._mode_str_model = ui.SimpleStringModel()
                     self._mode_str_model.as_string = self._mode
                     self._mode_model = ui.ComboBox(0, *MODES).model
                     self._mode_model_changed = self._mode_model.subscribe_item_changed_fn(self.on_item_changed)
-                ui.Spacer( height=10)
-            
+                
+                with ui.HStack():
+                    ui.Label("USD Url", name="usd_url", width=LABEL_WIDTH, height=HEIGHT)
+                    self._usd_url_model = ui.SimpleStringModel()
+                    self._usd_url_model.as_string = self._usd_file
+                    ui.StringField(model=self._usd_url_model, height=HEIGHT, word_wrap=True)
+                    self._usd_url_model_changed = self._usd_url_model.subscribe_value_changed_fn(self.on_value_changed)
+                
                 with ui.HStack():
                     ui.Label("Sharing Url", name="sharing_url", width=LABEL_WIDTH, height=HEIGHT)
                     self._sharing_url_model = ui.SimpleStringModel()
-                    self._sharing_url_model_label = ui.Label("", width=FIELD_WIDTH, word_wrap=True, alignment=ui.Alignment.TOP)
+                    self._sharing_url_model_label = ui.Label("", word_wrap=True, alignment=ui.Alignment.TOP)
                 
                 with ui.HStack():
                     ui.Spacer( width=LABEL_WIDTH)
-                    ui.Button("Copy Link", clicked_fn=self.copy_link_to_clipboard, width=LABEL_WIDTH, height=HEIGHT, alignment=ui.Alignment.RIGHT)
+                    ui.Button("Copy", clicked_fn=self.copy_url, width=60, height=HEIGHT)
+                    ui.Button("Test", clicked_fn=self.open_url, width=60, height=HEIGHT)
                 
-                ui.Spacer()
-                
+                with ui.HStack(style={"Label": {"color": cl("#058ef8")}}):
+                    ui.Spacer( width=LABEL_WIDTH)
+                    self._notification_label = ui.Label("", name="notification", height=HEIGHT)
+
         self.update_sharing_link()
-
-    def get_current_usd_file(self):
-        # Implement logic to fetch the currently opened USD file path
-        return "omniverse://localhost/Projects/Factory/Factory.usd"
-
-    def generate_link(self):
-        base_url = self._base_url_field.model.get_value()
-        app_id = self._app_id_field.model.get_value()
-        usd_file_link = self._usd_file_link_field.model.get_value()
-        stream_type = self._stream_type_dropdown.model.get_item_value(self._stream_type_dropdown.model.get_value())
-
-        # Validate USD file link
-        if not usd_file_link.startswith("omniverse://"):
-            self._generated_link_label.text = "Error: USD file link must start with omniverse://"
-            return
-        
-        # Construct the deep link
-        self._generated_link = f"{base_url}/app/{app_id}?file={usd_file_link}&mode={stream_type}"
-        self._generated_link_label.text = f"Generated Link: {self._generated_link}"
-
-    def copy_link_to_clipboard(self):
-        # Copy the generated link to clipboard
-        pyperclip.copy(self._generated_link)
-        print("Link copied to clipboard.")
-
-    def on_shutdown(self):
-        print("[de.innoactive] de innoactive shutdown")
