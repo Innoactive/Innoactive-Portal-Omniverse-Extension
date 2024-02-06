c0d62d02b9627046b1eb22ee21af8b04a229e813
Daniel Seidl <daniel.seidl@innoactive.de>
2024-02-06 22:44:52 +0100

    end of day

--- exts/de.innoactive/de/innoactive/extension.py
+++ exts/de.innoactive/de/innoactive/extension.py
@@ -12,6 +12,7 @@ from urllib.parse import quote
 
 LABEL_WIDTH = 120
 FIELD_WIDTH = 230
+BTN_SPACING = 5
 
 HEIGHT = 24
 
@@ -54,22 +55,30 @@ class DeInnoactiveExtension(omni.ext.IExt):
             print(f"Failed to copy text: {e}")
             return False
 
-    def set_notification(self, value):
-        self._notification_label.text = value
+    def set_notification(self, value, label):
+        label.text = value
+        label.visible = True
         def delete_notification():
-            self._notification_label.text = ""
+            label.text = ""
+            label.visible = False
 
         timer = threading.Timer(5, delete_notification)
         timer.start()
+    
+    def is_sharable_usd(self, file_path):
+        return file_path.startswith("omniverse://") or file_path.startswith("http://") or file_path.startswith("https://") # and not file_path.startswith("omniverse://localhost")
         
     def update_sharing_link(self):
-        args = quote(self._usd_url_model.as_string)
+        args = quote(self._usd_url_model.as_string, safe='')
         self._sharing_url_model.as_string = self._base_url_model.as_string + "/apps/" + self._app_id_model.as_string + "/launch/" + self._mode_str_model.as_string + "?args=" + args
         self._sharing_url_model_label.text = self._sharing_url_model.as_string
 
     def on_value_changed(self, item_model):
         self.update_sharing_link()
-        pass
+
+    def on_usd_value_changed(self, item_model):
+        self.update_sharing_link()
+        #self.button_copy.enabled = self.button_test.enabled = not self.is_sharable_usd(self._sharing_url_model.as_string)
 
     def on_item_changed(self, item_model, item):
         value_model = item_model.get_item_value_model(item)
@@ -78,83 +87,119 @@ class DeInnoactiveExtension(omni.ext.IExt):
 
         self.update_sharing_link()
 
-    def get_current_usd_file(self):
+    def clear_usd(self):
+        # Clear USD file from field
+        self._usd_url_model.as_string = ""
+
+    def set_stage_usd(self):
         # Implement logic to fetch the currently opened USD file path
         stage = omni.usd.get_context().get_stage()
         rootLayer = stage.GetRootLayer()
         file_path = rootLayer.realPath if rootLayer else ""
-        if file_path.startswith("omniverse://"):
-            #self.set_notification(f"USD file URL: {file_path}")
-            return file_path
+        if self.is_sharable_usd(file_path):
+           #self.set_notification(f"USD file URL: {file_path}")
+            self._usd_url_model.as_string = file_path
         else:
             #self.set_notification("The USD file URL does not start with 'omniverse://'.")
-            return ""
-        
+            self._usd_url_model.as_string = ""
+    
     def copy_url(self):
         # Copy the generated link to clipboard
-        self.copy_to_clipboard(self._sharing_url_model.as_string)
-        self.set_notification("Link copied to clipboard.")
+        if not self._usd_url_model.as_string:
+            self.set_notification("No USD file selected. Please select a valid omniverse:// or http(s):// USD file URL", self._warning_label)
+        elif not self.is_sharable_usd(self._usd_url_model.as_string):
+            self.set_notification("USD file is not shareable. Use omniverse:// or http(s):// format.", self._warning_label)
+        else:
+            self.copy_to_clipboard(self._sharing_url_model.as_string)
+            self.set_notification("Sharing link copied to clipboard.", self._notification_label)
+            
 
     def open_url(self):
         # Open URL in web browser
-        webbrowser.open_new_tab(self._sharing_url_model.as_string)
-        self.set_notification("Sharing URL opened in default webbrowser.")
+        if not self._usd_url_model.as_string:
+            self.set_notification("No USD file selected. Please select a valid omniverse:// or http(s):// USD file URL", self._warning_label)
+        elif not self.is_sharable_usd(self._usd_url_model.as_string):
+            self.set_notification("USD link is not shareable. Use omniverse:// or http(s):// format.", self._warning_label)
+        else:
+            webbrowser.open_new_tab(self._sharing_url_model.as_string)
+            self.set_notification("Sharing link opened in browser.", self._notification_label)
 
+    def open_invite_url(self):
+        # Open URL in web browser
+        invite_url = self._base_url + "/control-panel/v2/users"
+        webbrowser.open_new_tab(invite_url)
+        
     def on_shutdown(self):
         print("[de.innoactive] de innoactive shutdown")
 
     def on_startup(self, ext_id):
         print("Innoactive startup")
 
+        
         self._base_url = "https://portal.innoactive.io"
-        self._app_id = 1501  # Default app ID, to be provided
-        self._usd_file = self.get_current_usd_file()  # Implement this method
-        self._mode = "browser"  # Default to browser stream
+        self._app_id = 3757  # Default app ID, to be provided
+        self._usd_file = ""
+        self._mode = "cloud/browser"  # Default to browser stream
         self._generated_link = ""
 
-        self._window = ui.Window("Innoactive Portal", width=600, height=300)
+        self._window = ui.Window("Innoactive Portal", width=600, height=350)
         with self._window.frame:
-            with ui.VStack(spacing=10, height=0):
-                with ui.HStack():
+            with ui.VStack(spacing=8, height=0):
+                
+                with ui.HStack(spacing=5):
+                    manager = omni.kit.app.get_app().get_extension_manager()
+                    ext_path = manager.get_extension_path_by_module("de.innoactive")
+                    img = ui.Image(height=52,alignment=ui.Alignment.RIGHT)
+                    img.source_url = ext_path + "/data/innoactive_logo.png"
+                
+                with ui.HStack(spacing=5):
                     ui.Label("Base Url", name="base_url", width=LABEL_WIDTH, height=HEIGHT)
                     self._base_url_model = ui.SimpleStringModel()
                     self._base_url_model.as_string = self._base_url
                     ui.StringField(model=self._base_url_model, height=HEIGHT, word_wrap=True)
                     self._base_url_model_changed = self._base_url_model.subscribe_value_changed_fn(self.on_value_changed)
                 
-                with ui.HStack():
+                with ui.HStack(spacing=5):
                     ui.Label("App ID", name="app_id", width=LABEL_WIDTH, height=HEIGHT)
                     self._app_id_model = ui.SimpleIntModel()
                     self._app_id_model.as_int = self._app_id
                     ui.IntField(model=self._app_id_model, height=HEIGHT)
                     self._app_id_model_changed = self._app_id_model.subscribe_value_changed_fn(self.on_value_changed)
                 
-                with ui.HStack():
+                with ui.HStack(spacing=5):
                     ui.Label("Streaming Mode", name="mode", width=LABEL_WIDTH, height=HEIGHT)
                     self._mode_str_model = ui.SimpleStringModel()
                     self._mode_str_model.as_string = self._mode
                     self._mode_model = ui.ComboBox(0, *MODES).model
                     self._mode_model_changed = self._mode_model.subscribe_item_changed_fn(self.on_item_changed)
                 
-                with ui.HStack():
-                    ui.Label("USD Url", name="usd_url", width=LABEL_WIDTH, height=HEIGHT)
+                with ui.HStack(spacing=5):
+                    ui.Label("USD file", name="usd_url", width=LABEL_WIDTH, height=HEIGHT)
                     self._usd_url_model = ui.SimpleStringModel()
                     self._usd_url_model.as_string = self._usd_file
                     ui.StringField(model=self._usd_url_model, height=HEIGHT, word_wrap=True)
-                    self._usd_url_model_changed = self._usd_url_model.subscribe_value_changed_fn(self.on_value_changed)
+                    self._usd_url_model_changed = self._usd_url_model.subscribe_value_changed_fn(self.on_usd_value_changed)
+                    ui.Button("Clear", clicked_fn=self.clear_usd, width=60, height=HEIGHT)
+                    ui.Button("From Stage", clicked_fn=self.set_stage_usd, width=90, height=HEIGHT)
                 
-                with ui.HStack():
-                    ui.Label("Sharing Url", name="sharing_url", width=LABEL_WIDTH, height=HEIGHT)
+                with ui.HStack(spacing=5):
+                    ui.Label("Sharing URL", name="sharing_url", width=LABEL_WIDTH, height=HEIGHT)
                     self._sharing_url_model = ui.SimpleStringModel()
                     self._sharing_url_model_label = ui.Label("", word_wrap=True, alignment=ui.Alignment.TOP)
                 
-                with ui.HStack():
+                with ui.HStack(spacing=5):
                     ui.Spacer( width=LABEL_WIDTH)
-                    ui.Button("Copy", clicked_fn=self.copy_url, width=60, height=HEIGHT)
-                    ui.Button("Test", clicked_fn=self.open_url, width=60, height=HEIGHT)
+                    self.button_copy = ui.Button("Copy", clicked_fn=self.copy_url, width=60, height=HEIGHT)
+                    self.button_test = ui.Button("Test", clicked_fn=self.open_url, width=60, height=HEIGHT)
+                    self.button_invite = ui.Button("Invite user", clicked_fn=self.open_invite_url, width=90, height=HEIGHT)
+                    
+                with ui.HStack(spacing=5, style={"Label": {"color": cl("#76b900")}}):
+                    ui.Spacer( width=LABEL_WIDTH)
+                    self._notification_label = ui.Label("", name="notification", height=HEIGHT, visible=False)
                 
-                with ui.HStack(style={"Label": {"color": cl("#058ef8")}}):
+                with ui.HStack(spacing=5, style={"Label": {"color": cl("#d48f09")}}):
                     ui.Spacer( width=LABEL_WIDTH)
-                    self._notification_label = ui.Label("", name="notification", height=HEIGHT)
+                    self._warning_label = ui.Label("", name="notification", height=HEIGHT, visible=False)
 
         self.update_sharing_link()
+        self.set_stage_usd()
