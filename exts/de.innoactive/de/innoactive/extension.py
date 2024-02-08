1284d892b390f964083c6f35c015679e16a1a941
Daniel Seidl <daniel.seidl@innoactive.de>
2024-02-08 10:56:18 +0100

    updated docs

--- exts/de.innoactive/de/innoactive/extension.py
+++ exts/de.innoactive/de/innoactive/extension.py
@@ -12,12 +12,10 @@ import omni.usd
 from urllib.parse import quote
 
 LABEL_WIDTH = 120
-FIELD_WIDTH = 230
-BTN_SPACING = 5
-
 HEIGHT = 24
+VSPACING = 8
+HSPACING = 5
 
-SPACING = 4
 
 MODES = ("browser", "VR", "local")
 MODES_TECHNICAL = ("cloud/browser", "cloud/standalone", "local/windows")
@@ -25,9 +23,13 @@ MODES_TECHNICAL = ("cloud/browser", "cloud/standalone", "local/windows")
 APPS = ("Omniverse USD Composer 2023.2.3", "Omniverse USD Composer 2023.2.0")
 APP_IDS = (3757, 1501)
 
+DEFAULT_BASE_URL = "https://[yourcompany].innoactive.io"
+DEFAULT_APP_ID = 3757
+DEFAULT_MODE = "cloud/browser"
+
 settings = carb.settings.get_settings()
 
-# Functions and vars are available to other extension as usual in python: `example.python_ext.some_public_function(x)`
+# Public functions
 def get_sharing_link():
     print("[de.innoactive] get_sharing_link ")
     return self._sharing_url_model.as_string
@@ -111,9 +113,6 @@ class DeInnoactiveExtension(omni.ext.IExt):
         settings.set("/persistent/exts/de/innoactive/baseUrl", self._base_url_model.as_string)
         settings.set("/persistent/exts/de/innoactive/renderMode", self._mode_str_model.as_string)
         settings.set("/persistent/exts/de/innoactive/appId", self._app_id_model.as_int)
-        #print(f"self._app_id_model.as_int {self._app_id_model.as_int}")
-        #id = settings.get("/de/innoactive/appId")
-        #print(f"self._app_id_model.as_int saved {id}")
         
     def load_settings(self):
         self._base_url_model.as_string = settings.get("/persistent/exts/de/innoactive/baseUrl")
@@ -122,27 +121,19 @@ class DeInnoactiveExtension(omni.ext.IExt):
         
         # Defaults
         if self._base_url_model.as_string == "":
-            self._base_url_model.as_string = "https://company123.innoactive.io"
+            self._base_url_model.as_string = DEFAULT_BASE_URL
         
         if self._mode_str_model.as_string == "":
-            self._mode_str_model.as_string = "cloud/standalone"
+            self._mode_str_model.as_string = DEFAULT_MODE
         
         if self._app_id_model.as_int == 0:
-            self._app_id_model.as_int = 3757
-
-        #id = APP_IDS.index(1501)
-        #print(f"APP_IDS.index(1501) {id}")
-        print("persistent settings are stored in: {}".format(settings.get("/app/userConfigPath")))
-        #print(self._app_id_model.as_int)
-        #value_model = self._app_model.get_item_value_model()
-        #value_model.as_int = APP_IDS.index(self._app_id_model.as_int)
-        #print(value_model.as_int)
-        
+            self._app_id_model.as_int = DEFAULT_APP_ID
                 
     def clear_usd(self):
         # Clear USD file from field
         self._usd_url_model.as_string = ""
 
+        
     def set_stage_usd(self, at_autoload=False):
         # Implement logic to fetch the currently opened USD file path
         try:
@@ -150,7 +141,6 @@ class DeInnoactiveExtension(omni.ext.IExt):
             rootLayer = stage.GetRootLayer()
             file_path = rootLayer.realPath if rootLayer else ""
             if self.is_sharable_usd(file_path):
-            #self.set_notification(f"USD file URL: {file_path}")
                 self._usd_url_model.as_string = file_path
             else:
                 if not at_autoload: 
@@ -161,31 +151,33 @@ class DeInnoactiveExtension(omni.ext.IExt):
                 self.set_notification("Please load a valid omniverse:// or http(s):// USD file URL to your stage.", self._warning_label)
             self._usd_url_model.as_string = ""
     
-    def copy_url(self):
-        # Copy the generated link to clipboard
+    def validate_form(self):
         if not self._usd_url_model.as_string:
             self.set_notification("No USD file selected. Please select a valid omniverse:// or http(s):// USD file URL", self._warning_label)
         elif not self.is_sharable_usd(self._usd_url_model.as_string):
             self.set_notification("USD file is not shareable. Use omniverse:// or http(s):// format.", self._warning_label)
+        elif self._base_url_model.as_string == DEFAULT_BASE_URL:
+            self.set_notification("Configure Base URL to match your organization's Innoactive Portal domain name.", self._warning_label)
         else:
+            return True
+        return False
+    
+    def copy_url(self):
+        # Copy the generated link to clipboard
+        if self.validate_form():
             self.copy_to_clipboard(self._sharing_url_model.as_string)
             self.set_notification("Sharing link copied to clipboard.", self._notification_label)
             
 
     def open_url(self):
-        # Open URL in web browser
-        if not self._usd_url_model.as_string:
-            self.set_notification("No USD file selected. Please select a valid omniverse:// or http(s):// USD file URL", self._warning_label)
-        elif not self.is_sharable_usd(self._usd_url_model.as_string):
-            self.set_notification("USD link is not shareable. Use omniverse:// or http(s):// format.", self._warning_label)
-        else:
+        if self.validate_form():
             webbrowser.open_new_tab(self._sharing_url_model.as_string)
             self.set_notification("Sharing link opened in browser.", self._notification_label)
 
     def open_invite_url(self):
-        # Open URL in web browser
-        invite_url = self._base_url + "/control-panel/v2/users"
-        webbrowser.open_new_tab(invite_url)
+        if self.validate_form():
+            invite_url = self._base_url_model.as_string + "/control-panel/v2/users"
+            webbrowser.open_new_tab(invite_url)
         
     def on_shutdown(self):
         print("[de.innoactive] de innoactive shutdown")
@@ -198,13 +190,13 @@ class DeInnoactiveExtension(omni.ext.IExt):
         
         self._window = ui.Window("Innoactive Portal", width=600, height=350)
         with self._window.frame:
-            with ui.VStack(spacing=8, height=0):
+            with ui.VStack(spacing=VSPACING, height=0):
                 
-                with ui.HStack(spacing=5):
+                with ui.HStack(spacing=HSPACING):
                     img = ui.Image(height=40, alignment=ui.Alignment.RIGHT)
                     img.source_url = ext_path + "/data/innoactive_logo.png" 
                     
-                with ui.HStack(spacing=5):
+                with ui.HStack(spacing=HSPACING):
                     ui.Label("USD file", name="usd_url", width=LABEL_WIDTH, height=HEIGHT, tooltip="Ensure the USD file is hosted on Nucleus and the user with whom you want to share access has permissions to access that file on Nucleus Server.")
                     self._usd_url_model = ui.SimpleStringModel()
                     self._usd_url_model.as_string = ""
@@ -212,14 +204,14 @@ class DeInnoactiveExtension(omni.ext.IExt):
                     self._usd_url_model_changed = self._usd_url_model.subscribe_value_changed_fn(self.on_usd_value_changed)
                     ui.Button("From Stage", clicked_fn=self.set_stage_usd, width=90, height=HEIGHT, tooltip="Use the currently loaded USD file from Stage")
                 
-                with ui.HStack(spacing=5):
+                with ui.HStack(spacing=HSPACING):
                     ui.Label("Runtime", name="app", width=LABEL_WIDTH, height=HEIGHT, tooltip="Select the OV Kit runtime you want to use. You can upload your own runtimes, please contact Innoactive support.")
                     self._app_id_model = ui.SimpleStringModel()
                     self._app_id_model.as_int = settings.get("/persistent/exts/de/innoactive/appId")
                     self._app_model = ui.ComboBox(APP_IDS.index(self._app_id_model.as_int), *APPS).model
                     self._app_model_changed = self._app_model.subscribe_item_changed_fn(self.on_app_changed)
                 
-                with ui.HStack(spacing=5):
+                with ui.HStack(spacing=HSPACING):
                     ui.Label("Streaming Mode", name="mode", width=LABEL_WIDTH, height=HEIGHT, tooltip="Select weather the link shall start a browser stream, VR stream or a locally rendered session")
                     self._mode_str_model = ui.SimpleStringModel()
                     self._mode_str_model.as_string = settings.get("/persistent/exts/de/innoactive/renderMode")
@@ -227,7 +219,7 @@ class DeInnoactiveExtension(omni.ext.IExt):
                     self._mode_model = ui.ComboBox(MODES_TECHNICAL.index(self._mode_str_model.as_string), *MODES).model
                     self._mode_model_changed = self._mode_model.subscribe_item_changed_fn(self.on_mode_changed)
                 
-                with ui.HStack(spacing=5):
+                with ui.HStack(spacing=HSPACING):
                     ui.Label("Base Url", name="base_url", width=LABEL_WIDTH, height=HEIGHT, tooltip="Set this to your match your Innoactive Portal cloud domain URL")
                     self._base_url_model = ui.SimpleStringModel()
                     self._base_url_model.as_string = settings.get("/persistent/exts/de/innoactive/baseUrl")
@@ -236,20 +228,20 @@ class DeInnoactiveExtension(omni.ext.IExt):
         
                 ui.Line()
 
-                with ui.HStack(spacing=5):
+                with ui.HStack(spacing=HSPACING):
                     ui.Label("Sharing URL", name="sharing_url", width=LABEL_WIDTH, height=HEIGHT, tooltip="Copy and share this link with a user. You need to invite the user to Innoactive Portal as well.")
                     self._sharing_url_model = ui.SimpleStringModel()
                     self._sharing_url_model_label = ui.Label("", word_wrap=True, alignment=ui.Alignment.TOP)
                 
-                with ui.HStack(spacing=5):
+                with ui.HStack(spacing=HSPACING):
                     ui.Spacer( width=LABEL_WIDTH)
                     self.button_copy = ui.Button("Copy", clicked_fn=self.copy_url, width=60, height=HEIGHT, tooltip="Copy the sharing link to the clipboard")
                     self.button_test = ui.Button("Test", clicked_fn=self.open_url, width=60, height=HEIGHT, tooltip="Test the sharink link on your PC")
                     self.button_invite = ui.Button("Invite user", clicked_fn=self.open_invite_url, width=90, height=HEIGHT, tooltip="Invite a user to Innoactive Portal")
                     
-                with ui.HStack(spacing=5, style={"Notification": {"color": cl("#76b900")}, "Error": {"color": cl("#d48f09")}}):
+                with ui.HStack(spacing=HSPACING, style={"Notification": {"color": cl("#76b900")}, "Error": {"color": cl("#d48f09")}}):
                     ui.Spacer( width=LABEL_WIDTH)
-                    with ui.VStack(spacing=8, height=0):
+                    with ui.VStack(spacing=0, height=0):
                         self._notification_label = ui.Label("", word_wrap=True, name="notification", height=HEIGHT, visible=False, style_type_name_override="Notification")
                         self._warning_label = ui.Label("", word_wrap=True, name="notification", height=HEIGHT, visible=False, style_type_name_override="Error")
 
